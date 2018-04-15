8fff559f5077f6d79bfe0b3b7f0bc2377f185156
Moritz Sternemann <moritz@strnmn.me>
2018-04-15 15:43:36 +0200

    Initial commit

---
+++ dist/nuxt-cloudkit.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function default_1(config) {
+    console.log('nuxt-cloudkit:', config);
+}
+exports.default = default_1;
+;
